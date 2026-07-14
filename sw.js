6edd155be621a4a13764a9b0da4e38a40d8c9566
Nusaindotek <nusaindoteknologi@gmail.com>
2026-07-14 19:32:54 +0700

    Update service worker cache version and URLs

--- sw.js
+++ sw.js
@@ -1,16 +1,21 @@
-const CACHE_NAME = "nusapos-v1.1";
+const CACHE_NAME = "nusapos-v1.2";
 const urlsToCache = [
-  "./index.html",
-  "./setting.html",
-  "./app.js",
-  "./icon.png",
-  "./icon_192.png",
-  "./icon_512.png",
-  "./manifest.json"
+  "index.html",
+  "setting.html",
+  "app.js",
+  "icon.png",
+  "icon_192.png",
+  "icon_512.png",
+  "manifest.json"
 ];
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener("fetch", event => {
