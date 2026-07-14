939468ec69c03ab9a374dbe1337c5adeb0116465
Nusaindotek <nusaindoteknologi@gmail.com>
2026-07-14 19:44:33 +0700

    Refactor service worker for caching strategy

--- sw.js
+++ sw.js
@@ -1,23 +1,27 @@
-const CACHE_NAME = "nusapos-v1.2";
-const urlsToCache = [
-  "index.html",
-  "setting.html",
-  "app.js",
-  "icon.png",
-  "icon_192.png",
-  "icon_512.png",
-  "manifest.json"
+const CACHE_NAME = "nusapos-kasir-v1.0";
+const URLS_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon.png",
+  "./icon_192.png",
+  "./icon_512.png",
+  "./app.js"
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
+  );
   self.skipWaiting();
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
 
-self.addEventListener("activate", event => {
-  event.waitUntil(self.clients.claim());
+self.addEventListener("activate", (e) => {
+  e.waitUntil(self.clients.claim());
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
 });
